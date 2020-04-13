72733186c249a1e090105f21d55464ea2c4f355b
Erick salvador garcia mora <erick_mora92@hotmail.com>
2020-04-13 18:53:06 -0500

    Django

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -1,15 +1,9 @@
 """
 Definition of urls for DjangoWeb.
-"""
-
 from datetime import datetime
-from django.urls import path
-from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.contrib.auth.views import LoginView, LogoutView
 
-
-urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
@@ -26,5 +20,12 @@ urlpatterns = [
          ),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('blog.urls')),
 ]
